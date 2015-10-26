643f01557ac3c489ad6ce998d4d1bf55a4063178
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-10-26 14:26:10 +0100

    Remove duplicate migration file

--- db/migrate/20151023142812_change_product_fields.rb
+++
@@ -1,6 +0,0 @@
-class ChangeProductFields < ActiveRecord::Migration
-  def change
-  	change_column :products, :title, :string, null:false
-  	change_column :products, :price, :string, null:false
-  end
-end
