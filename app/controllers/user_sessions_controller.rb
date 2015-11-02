3457fdf0f630403ec3ea9b4144ae8380de1589d5
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-11-02 09:46:51 +0100

    Add style for login page

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,15 +10,14 @@ class UserSessionsController < ApplicationController
 
   def create
   	user = User.find_by(email: params[:email])
+
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.id
       redirect_to user
   		flash[:success] = "Thanks for logging in!"
-     
   	else
       flash[:success] = "There was a problem logging in!"
   		render action: 'new'
-  		
   	end
   end
 end
