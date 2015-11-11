fc01da94062440042d479324bc375e33fd1d3920
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-11-11 21:43:44 +0100

    Validated password only on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   rolify
 	has_secure_password
-	validates :password, presence:true
+	validates :password, presence:true, :on => :create
 	validates :full_name, presence:true
 	validates :email, presence: true, uniqueness: true,
 										format: {
