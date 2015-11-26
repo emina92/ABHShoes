16ea1b9967b199cbb40149727137c2c3e0617365
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-11-26 13:30:21 +0100

    Add filters by price and brand

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
-  def show
-  	 @category = Category.find(params[:id])
-  end
+	def show
+		@category = Category.find(params[:id])
+		@brand = Brand.find(params[:id])
+		@min_price = params[:min_price]
+		@max_price = params[:max_price]
+		@brand_id = params[:brand_id]
+		@products = @category.products
+		@products = @products.where("price > ?", @min_price) if params[:min_price].present?
+		@products = @products.where("price < ?", @max_price) if params[:max_price].present?
+		@products = @products.where("brand_id = ?", @brand_id) if params[:brand_id].present?
+	end
 end
