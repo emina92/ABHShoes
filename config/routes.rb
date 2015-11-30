95fec197697ca975a1d5f2b1742fa4b0b34338ec
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-11-30 10:48:28 +0100

    Delete route 'categories/show'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'store/:id' => 'store#show', :as => :store_product
 
-  get 'categories/show'
-
   get 'login'   => 'user_sessions#new'
   get 'logout'  => 'user_sessions#destroy'
 
