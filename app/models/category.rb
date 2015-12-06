98f4ce9f3539c7b6cd2accee9dbf920283a0a75b
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-12-06 15:04:44 +0100

    Fix bug for category form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
 	has_many :products
 
-	validates :name, presence:true
+	validates :name, presence:true, length: { in: 3..20 }
+	validates :description, length: { maximum: 50 }
 end
