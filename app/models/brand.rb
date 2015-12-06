a8e95d640b194f5afd19f24db7fa7bef3a668af7
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-12-06 15:03:51 +0100

    Fix bugs for brand form

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,6 @@
 class Brand < ActiveRecord::Base
 	has_many :products
 
-	validates :name, presence:true
+	validates :name, presence:true, length: { in: 2..20 }
+	validates :description, length: { maximum: 50 }
 end
