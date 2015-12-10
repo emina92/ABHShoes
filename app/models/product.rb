12b2ee81d8e97c82dfbc3a53fcbc59dd0afbca15
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-12-10 13:30:05 +0100

    Add link to product cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
 	belongs_to :category
   belongs_to :brand
   has_many :product_variants
+  has_many :cart_items
 
   accepts_nested_attributes_for :product_variants, allow_destroy: true
 
