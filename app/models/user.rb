e37c6b74338dd9bcd65f603d303d200590a87e70
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-12-15 17:34:19 +0100

    Add model ShippingAddress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :cart_items
-  rolify
+	has_many :shipping_addresses
+  	rolify
 	has_secure_password
 	validates :password, presence:true, :on => :create, length: { in: 6..20 }
 	validates :full_name, presence: true, length: { in: 6..50 }
@@ -11,11 +12,16 @@ class User < ActiveRecord::Base
 	def total_quantity
 		self.cart_items.sum(:quantity)
 	end
+
 	def total_price
 		total = 0
 		self.cart_items.each do |item|
 			total += item.quantity * item.product.price
 		end
 		return total
+	end
+
+	def total_with_shipping
+		total_price + 10
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208142147) do
+ActiveRecord::Schema.define(version: 20151213173242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,19 @@ ActiveRecord::Schema.define(version: 20151208142147) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
+
+  create_table "shipping_addresses", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "country"
+    t.string   "phone_number"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
