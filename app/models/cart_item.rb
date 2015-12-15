5fc394524e74076c085cba2a10586723482d5da3
Emina Muharemovic <emuharemov2@etf.unsa.ba>
2015-12-15 17:44:03 +0100

    Add validation for quantity

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,7 @@
 class CartItem < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :product
+	validates :quantity, presence:true
 
 	def self.quantity_options
     	(1..10).to_a.collect{|p| ["#{p}", p]}
